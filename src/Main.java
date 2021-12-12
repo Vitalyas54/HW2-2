ce4f986489b7d052fbc9bbd832bec7e0a9b7027e
Vitaliy Bozhko <raptor1111@list.ru>
2021-12-12 20:45:46 +0700

    Code reformated

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int startBalance = 200;
         int balanceOffer = 100;
-        int bonus = balanceOffer/100;
+        int bonus = balanceOffer / 100;
         int totalBalance;
-        if (balanceOffer >= 100){
+        if (balanceOffer >= 100) {
             totalBalance = balanceOffer + bonus + startBalance;
             System.out.println("Ваш баланс равен " + totalBalance + " рублей." + " В том числе Вам начислено " + bonus + " бонусных рублей.");
-        }
-        else {
+        } else {
             totalBalance = balanceOffer + startBalance;
             System.out.println("Ваш баланс равен " + totalBalance + " рублей.");
         }
